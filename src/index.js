2b8bdcb0ea1d5b5ceca613524612af92cfd80da9
Dave Barbalato <dbarbalato@gmail.com>
2019-02-10 23:21:21 -0500

    Build

--- src/index.js
+++
@@ -1,7 +0,0 @@
-require('./assets/js/script.js');
-require('./assets/scss/style.scss');
-require('./pages/main.ejs');
-require('./pages/jobs.ejs');
-require('./pages/news.ejs');
-require('./partials/links.ejs');
-require('./data/stats.json');
